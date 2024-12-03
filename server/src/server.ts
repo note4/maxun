27148072dcf1b25627b6691768eea60e55675c1c
amhsirak <karishmashuklaa@gmail.com>
2024-12-03 21:41:21 +0530

    feat: set credentials to true

--- server/src/server.ts
+++ server/src/server.ts
@@ -95,6 +95,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', process.env.PUBLIC_URL || 'http://localhost:5173');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
   if (req.method === 'OPTIONS') {
     return res.sendStatus(200);
   }
